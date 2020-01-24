93c9af441673c3583a9e53c30021755238314ca4
Priyam Patel <patelpriyam090@gmail.com>
2020-01-24 23:29:30 +0530

    feat: firebase updation

--- _webpack/webpack.dev.js
+++ _webpack/webpack.dev.js
@@ -2,7 +2,6 @@ const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const path = require('path');
 
-
 module.exports = merge(commonConfig, {
   mode: 'development',
   devServer: {
